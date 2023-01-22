1086421a5f23fd21ec03be27bf01791d92f4ca3c
AkmalDjumayev <akmald77@gmail.com>
2023-01-22 10:34:07 +0500

    4. Залейте код в репозиторий, оставьте ссылку на коммит.

---
+++ src/main/java/ru/job4j/condition/ThreeMax.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+public class ThreeMax {
+    public static int max(int first, int second, int third) {
+        int result = first;
+        if (first >= second && first >= third) {
+            result = first;
+            return result;
+        }
+        if (second >= first && second >= third) {
+            result = second;
+        } else {
+            result = third;
+        }
+        return result;
+    }
+}
+
