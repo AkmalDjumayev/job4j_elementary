e4b459625558a516bc54d8da3697983a4c37b537
AkmalDjumayev <akmald77@gmail.com>
2023-02-05 12:11:15 +0500

    6.9. String - это массив [#504884]

--- src/main/java/ru/job4j/array/JavaNameValidator.java
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -4,20 +4,20 @@ public class JavaNameValidator {
 
      public static boolean isNameValid(String name) {
         boolean rsl = false;
-        if (name.isEmpty() || (isUpperLatinLetter(name.charAt(0)))
-                || (Character.isDigit(name.charAt(0)))) {
-            return false;
-        } else {
-            for (int i = 1; i < name.length(); i++) {
-                rsl = ((isSpecialSymbol(name.charAt(i))) || (isUpperLatinLetter(name.charAt(i)))
-                        || (isLowerLatinLetter(name.charAt(i))) || (Character.isDigit(name.charAt(i))));
-                if (!rsl) {
-                    break;
-                }
-            }
-        }
-        return rsl;
-    }
+         if (name.isEmpty() || (isUpperLatinLetter(name.charAt(0)))
+                 || (Character.isDigit(name.charAt(0)))) {
+             return rsl;
+         }
+         for (int i = 1; i < name.length(); i++) {
+             int sim = name.charAt(i);
+             rsl = ((isSpecialSymbol(sim)) || (isUpperLatinLetter(sim))
+                     || (isLowerLatinLetter(sim) || (Character.isDigit(sim))));
+             if (!rsl) {
+                 break;
+             }
+         }
+         return rsl;
+     }
 
     public static boolean isSpecialSymbol(int code) {
         return (code == 36) || (code == 95);
